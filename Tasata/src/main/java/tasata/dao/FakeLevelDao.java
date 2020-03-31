623c17f45419a98c2065413e55d51d6be4607999
Juha Kauppinen <juha.kauppinen@helsinki.fi>
2020-03-31 19:08:55 +0300

    Vähän hankalampi demo

--- Tasata/src/main/java/tasata/dao/FakeLevelDao.java
+++ Tasata/src/main/java/tasata/dao/FakeLevelDao.java
@@ -12,17 +12,56 @@ public class FakeLevelDao implements LevelDao {
     public Level findLevelById(String id) {
         Level level = new Level(id);
         
-        level.addTile(new Tile(1, 4));
+//        level.addTile(new Tile(0, 4));
+//        level.addTile(new Tile(1, 0));
+//        level.addTile(new Tile(2, 0));
+//        level.addTile(new Tile(3, 0));
+//        
+//        level.createTileConnections(new int[][]{
+//            {0,1,3},
+//            {1,2,1},
+//            {2,3,3}
+//        });
+        
+        level.addTile(new Tile(0, 6));
+        level.addTile(new Tile(1, 0));
         level.addTile(new Tile(2, 0));
         level.addTile(new Tile(3, 0));
         level.addTile(new Tile(4, 0));
+        level.addTile(new Tile(5, 0));
         
         level.createTileConnections(new int[][]{
-            {1,2,3},
-            {2,3,1},
-            {3,4,3}
+            {0,1,3},
+            {1,2,1},
+            {1,3,2},
+            {3,4,1},
+            {3,5,3}
         });
-        
+
+//        level.addTile(new Tile(0, 0));
+//        level.addTile(new Tile(1, 10));
+//        level.addTile(new Tile(2, 0));
+//        level.addTile(new Tile(3, 0));
+//        level.addTile(new Tile(4, 0));
+//        level.addTile(new Tile(5, 0));
+//        level.addTile(new Tile(6, 0));
+//        level.addTile(new Tile(7, 0));
+//        level.addTile(new Tile(8, 0));
+//        level.addTile(new Tile(9, 0));
+//        
+//        level.createTileConnections(new int[][]{
+//            {0,1,3},
+//            {1,2,4},
+//            {1,3,1},
+//            {3,4,3},
+//            {1,4,2},
+//            {4,5,3},
+//            {5,6,1},
+//            {5,7,4},
+//            {7,8,5},
+//            {7,9,4}
+//        });
+            
         return level;
     }
     
