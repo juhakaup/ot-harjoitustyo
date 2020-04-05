7c1fe6c8a223ffba4608ad109b8db416a213cd71
Juha Kauppinen <juha.kauppinen@helsinki.fi>
2020-04-05 07:50:55 +0300

    fileLevelDao palauttaa kopion levelistä

--- Tasata/src/main/java/tasata/dao/FileLevelDao.java
+++ Tasata/src/main/java/tasata/dao/FileLevelDao.java
@@ -6,44 +6,57 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import tasata.domain.Level;
+import tasata.domain.Tile;
 
 /**
- *
+ * This class handles the level loading from a file
  */
 
 public class FileLevelDao implements LevelDao {
+
     private static File file;
     private Level[] levels;
-    private static Gson gson = new Gson();
+    private static final Gson gson = new Gson();
 
     public FileLevelDao(String fileLocation) throws Exception {
         file = new File(fileLocation);
-        if(!file.exists()) {
+        if (!file.exists()) {
             System.out.println("No such file");
         }
-        
+
         InputStreamReader reader;
         try {
             reader = new InputStreamReader(new FileInputStream(file), "UTF-8");
             JsonReader jsonReader = new JsonReader(reader);
             levels = gson.fromJson(jsonReader, Level[].class);
-            
-            for(int i=0; i < levels.length; i++) {
+
+            for (int i = 0; i < levels.length; i++) {
                 levels[i].createTileConnections();
             }
-            
+
         } catch (Exception e) {
             System.out.println("error in file level dao");
         }
     }
-    
+
     @Override
     public Level findLevelById(String id) {
-        for(Level level : levels) {
-            if(level.equals(id)) {
-                return level;
+        for (Level level : levels) {
+            if (level.equals(id)) {
+                return createCopy(level);
             }
         }
         return null;
+    }
+
+    private Level createCopy(Level level) {
+        Level newLevel = new Level(level.getId());
+        
+        level.getTileSet().forEach((t) -> {
+            newLevel.addTile(new Tile(t.getId(), t.getValue()));
+        });
+        newLevel.createTileConnections(level.getConnections());
+
+        return newLevel;
     }
 }
