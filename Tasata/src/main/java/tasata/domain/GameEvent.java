3ee1df7804914ecf4036481edf400bf69700f8e8
Juha Kauppinen <juha.kauppinen@helsinki.fi>
2020-04-14 17:43:22 +0300

    Lisätty eventtejä

--- Tasata/src/main/java/tasata/domain/GameEvent.java
+++ Tasata/src/main/java/tasata/domain/GameEvent.java
@@ -4,9 +4,12 @@ package tasata.domain;
 public enum GameEvent {
     LOAD_LEVEL, 
     RESET_LEVEL, 
-    MENU_SCENE, 
-    STATE_CHANGE, 
+    TILE_CHANGE, 
+    LEVEL_SOLVED,
+    LEVEL_LOADED,
+    LEVEL_STATE_CHANGE,
     TILE_PRESS,
+    MENU_SCENE, 
     NEXT_LEVEL,
     POPUP_MENU
 }
